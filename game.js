cbf4784063bbb010551587d23eeb5077c1c5830a
kmeitz <kcmeit16@smumn.edu>
2019-09-28 16:14:21 -0500

    Hiding the button and input field when they win the game. Added in a scoreboard. Added in a spinner but couldn't figure out how to get it to start and stop

--- game.js
+++ game.js
@@ -1,31 +1,69 @@
 var y = Math.floor(Math.random() * 100 + 1); 
-      
-   
-    var guess = 1; 
+var guess = 0; 
+
+
+function hide() {
+    var x = document.getElementById("guess");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    var w = document.getElementById("button");
+    if (w.style.display === "none") {
+      w.style.display = "block";
+    } else {
+      w.style.display = "none";
+    }
+  }
+
+function newGame() {
+	secretNumber = Math.floor(Math.random() * 100) + 1;
+    numberOfGuesses = 0;
+    document.getElementById("score").innerHTML = ""
+}
+	
     
-      
-       
-   document.getElementById("button").onclick = function (){
-       
+
+
+   document.getElementById("button").onclick = function game(){
+    
+    var x = document.getElementById("restart");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    var w = document.getElementById("button");
+    if (w.style.display === "none") {
+      w.style.display = "block";
+    } else {
+      w.style.display = "none";
+
     var number = document.getElementById("guess").value;
-  
+
    if(number == y) 
    {     
-    function myFunction() {
-        document.getElementById("guess").innerHTML = "Paragraph changed!";
-      }
+    setTimeout("Hello",3000);
+    guess++
+    document.getElementById("correct").innerHTML = "Congratulations! It took you " + guess + " attempts to guess the number correctly.";
+    document.getElementById("score").innerHTML = guess;
+    hide();
+    newGame();
+    
 
-        
    } 
    else if(number > y) 
-   {     
+   {   setTimeout("Hello",3000);
        guess++; 
-       alert("Wrong, the number is smaller.") ;
+       document.getElementById("correct").innerHTML = "Incorrect, the correct number is smaller than " + number ;
+       document.getElementById("score").innerHTML = guess;
    } 
    else
-   { 
+   {    setTimeout("Hello",3000);
        guess++; 
-       alert("Wrong, the number is bigger")
-       
+       document.getElementById("correct").innerHTML = "Incorrect, the correct number is bigger than " + number ;
+       document.getElementById("score").innerHTML = guess;
+
    } 
-}
+} 
