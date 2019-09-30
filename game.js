5d6f451029507f6312f5e9c085bab754d403e64b
kmeitz <kcmeit16@smumn.edu>
2019-09-30 13:02:41 -0500

    restart

--- game.js
+++ game.js
@@ -1,99 +1,89 @@
 document.getElementById("restart").style.visibility = "hidden";
-var y = Math.floor(Math.random() * 100 + 1); 
-var guess = 0; 
+var y = Math.floor(Math.random() * 100 + 1);
+var guess = 0;
 
 
 function hide() {
     var x = document.getElementById("guess");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
     var w = document.getElementById("button");
     if (w.style.display === "none") {
-      w.style.display = "block";
+        w.style.display = "block";
     } else {
-      w.style.display = "none";
+        w.style.display = "none";
     }
-  }
+}
 
-  
+//
 
 function newGame() {
-	secretNumber = Math.floor(Math.random() * 100) + 1;
+    secretNumber = Math.floor(Math.random() * 100) + 1;
     numberOfGuesses = 0;
     document.getElementById("score").innerHTML = ""
 
 
 }
-	
-    
 
 
-   document.getElementById("button").onclick = function game(){
-    
-    
-    
+
+
+document.getElementById("button").onclick = function game() {
+
+
+
 
     var number = document.getElementById("guess").value;
 
-   if(number == y) 
-   {     
-    
-    guess++
-    document.getElementById("correct").innerHTML = "Congratulations! It took you " + guess + " attempts to guess the number correctly.";
-    document.getElementById("score").innerHTML = guess;
-    document.getElementById("restart").style.visibility = "visible";
-    newGame();
-    hide();
-    
-    
+    if (number == y) {
 
-   } 
-   else if(number > y) 
-   {   
-       guess++; 
-       document.getElementById("correct").innerHTML = "Incorrect, the correct number is smaller than " + number ;
-       document.getElementById("score").innerHTML = guess;
-   } 
-   else
-   {    
-       guess++; 
-       document.getElementById("correct").innerHTML = "Incorrect, the correct number is bigger than " + number ;
-       document.getElementById("score").innerHTML = guess;
+        guess++
+        document.getElementById("correct").innerHTML = "Congratulations! It took you " + guess + " attempts to guess the number correctly.";
+        document.getElementById("score").innerHTML = guess;
+        document.getElementById("restart").style.visibility = "visible";
+        newGame();
+        hide();
 
-   } 
-} 
 
-document.getElementById("restart").onclick = function restart(){
-  document.getElementById("restart").style.visibility = "hidden";
-  var number = document.getElementById("guess").value;
 
-  if(number == y) 
-  {     
-   
-   guess++
-   document.getElementById("correct").innerHTML = "Congratulations! It took you " + guess + " attempts to guess the number correctly.";
-   document.getElementById("score").innerHTML = guess;
-   document.getElementById("restart").style.visibility = "visible";
-   newGame();
-   hide();
-   
-   
+    } else if (number > y) {
+        guess++;
+        document.getElementById("correct").innerHTML = "Incorrect, the correct number is smaller than " + number;
+        document.getElementById("score").innerHTML = guess;
+    } else {
+        guess++;
+        document.getElementById("correct").innerHTML = "Incorrect, the correct number is bigger than " + number;
+        document.getElementById("score").innerHTML = guess;
 
-  } 
-  else if(number > y) 
-  {   
-      guess++; 
-      document.getElementById("correct").innerHTML = "Incorrect, the correct number is smaller than " + number ;
-      document.getElementById("score").innerHTML = guess;
-  } 
-  else
-  {    
-      guess++; 
-      document.getElementById("correct").innerHTML = "Incorrect, the correct number is bigger than " + number ;
-      document.getElementById("score").innerHTML = guess;
+    }
+}
 
-  } 
+document.getElementById("restart").onclick = function restart() {
+    document.getElementById("restart").style.visibility = "hidden";
+    var number = document.getElementById("guess").value;
+
+    if (number == y) {
+
+        guess++
+        document.getElementById("correct").innerHTML = "Congratulations! It took you " + guess + " attempts to guess the number correctly.";
+        document.getElementById("score").innerHTML = guess;
+        document.getElementById("restart").style.visibility = "visible";
+        newGame();
+        hide();
+
+
+
+    } else if (number > y) {
+        guess++;
+        document.getElementById("correct").innerHTML = "Incorrect, the correct number is smaller than " + number;
+        document.getElementById("score").innerHTML = guess;
+    } else {
+        guess++;
+        document.getElementById("correct").innerHTML = "Incorrect, the correct number is bigger than " + number;
+        document.getElementById("score").innerHTML = guess;
+
+    }
 }
